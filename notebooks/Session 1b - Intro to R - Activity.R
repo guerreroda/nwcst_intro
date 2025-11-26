4d6112acfb7d36a6c4b2c7ae1d8c4e53f43d0b99
Diego Guerrero <guerreroda@gmail.com>
2025-11-26 09:01:00 -0500

    Add files via upload

--- notebooks/Session 1b - Intro to R - Activity.R
+++ notebooks/Session 1b - Intro to R - Activity.R
@@ -3,32 +3,43 @@
 # ACTIVITY SOLUTION
 # =============================================================================
 
-# 1. Read (import) both files
+# For the activity replace the XXX with the corresponding code
+
+# Load libraries
+# Load required packages
+library(tidyverse)
+library(lubridate)
+library(openxlsx)
+
+# 1. Read (import) both files, "us_gdp.xlsx" and "us_stocks.xlsx":
 us_gdp <- read.xlsx("us_gdp.xlsx")
-us_stocks <- read.xlsx("us_stocks.xlsx")
+us_gdp
+
+# Complete for us_stocks:
+us_stocks
+us_stocks
 
 # 2. Clean the data (check date columns data types)
-us_gdp_clean <- us_gdp %>%
+us_gdp <- us_gdp %>%
   mutate(date = make_date(date))
 
-us_stocks_clean <- us_stocks %>%
-  mutate(date = make_date(date))
+us_stocks <- XXX %>%
+  XXX(date = make_date(date))
 
 # 3. Join the data frames
-us_merged <- us_gdp_clean %>%
-  left_join(us_stocks_clean, by = "date")
+us_merged <- left_join(us_stocks_clean, XXX, by = "date")
 
 # 4. Obtain the average and standard deviation of the NASDAQ
 nasdaq_stats <- us_merged %>%
   summarise(
-    avg_nasdaq = mean(nasdaq, na.rm = TRUE),
-    sd_nasdaq = sd(nasdaq, na.rm = TRUE)
+    avg_nasdaq = mean(nasdaq),
+    sd_nasdaq = sd(XXX)
   )
 
 nasdaq_stats
 
 # 5. Make a scatter plot
-ggplot(us_merged, aes(x = nasdaq, y = real_gdp)) +
+ggplot(us_merged, aes(x = XXX, y = XXX)) +
   geom_point(color = "darkblue", size = 3, alpha = 0.7) +
   geom_smooth(method = "lm", se = TRUE, color = "red") +
   labs(
@@ -39,4 +50,4 @@ ggplot(us_merged, aes(x = nasdaq, y = real_gdp)) +
   theme_minimal()
 
 # 6. Write (export) the merged data frame as a .xlsx file
-write.xlsx(activity_merged, "us_merged.xlsx")
+write.xlsx(XXX, "us_merged.xlsx")
